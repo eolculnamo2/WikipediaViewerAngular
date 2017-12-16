723039154d2386b5a6504eb933779309953639fc
eolculnamo2 <rbertram8@gmail.com>
2017-12-16 02:27:27 -0600

    fetch

--- public/client.js
+++ public/client.js
@@ -2,5 +2,12 @@
 function go(){
     var search = document.getElementById("search").value;
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search +"&format=json&origin=*";
-    alert(search);
+    
+    fetch(url)
+    .then((response)=>{
+        response.json();
+    })
+    .then((search)=>{
+        console.log(search);
+    })
 }
